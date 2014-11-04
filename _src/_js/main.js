31fa368c619f72ebbd0b925298b34698c5877a70
Lucas <lucas@agtlucas.com>
2014-11-04 16:20:47 -0200

    Updates

---
+++ _src/_js/main.js
@@ -0,0 +1,47 @@
+/**
+ * Blackbriar
+ * Base JS
+ */
+var $ = require('jquery');
+
+var Blackbriar = (function (window, document, undefined) {
+
+  'use strict';
+
+  /**
+   * Blackbriar object
+   */
+  var Blackbriar = {};
+
+  /**
+   * Init Blackbriar program
+   * Initialize other methods
+   */
+  Blackbriar.init = function () {
+    this.toggleMenu();
+  };
+
+  /**
+   * Toggle Menu when tablet && mobile
+   * When click on menu button, show/hide the navigation menu
+   */
+  Blackbriar.toggleMenu = function () {
+    $(document).on('click', '.nav-toggle', function () {
+      $('.site-nav').slideToggle();
+    });
+  };
+
+  /**
+   * Detect if is mobile
+   */
+  Blackbriar.isMobile = function () {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  return Blackbriar.init();
+
+})(window, document);
